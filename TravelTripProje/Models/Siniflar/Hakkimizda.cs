dbc1cd193e1a8a948b2558c06b3584e64fec35f4
ceydakyaa <88967224+ceydakyaa@users.noreply.github.com>
2025-04-17 23:20:38 +0300

    Add project files.

---
+++ TravelTripProje/Models/Siniflar/Hakkimizda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Hakkimizda
+    {
+        public int ID { get; set; }
+        public string FotoUrl { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
